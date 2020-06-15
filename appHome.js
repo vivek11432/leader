9895a60d4fa942dceec6ed471ee85811d8ec961c
Vivek Kumar <vivekgari@gmail.com>
2020-06-15 18:18:51 +0530

    Adding lead, assigning mechanic, db setup

--- appHome.js
+++ appHome.js
@@ -3,7 +3,61 @@ const qs = require("qs");
 const dotenv = require('dotenv');
 dotenv.config();
 const apiUrl = process.env.API_URL;
+const SLACK_OAUTH_TOKEN = process.env.SLACK_OAUTH_TOKEN;
+var request = require('request');
 const displayHome = async (user, data) => {
+  var dynamicBlock = [];
+  for(var ii = 0; ii < data.length; ii++){
+    dynamicBlock = dynamicBlock.concat([
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `*Lead - #${data[ii].id}*`
+        }
+      },
+      {
+        "type": "divider"
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `*Created by* - @${data[ii].created_by_name}\n\n*Mechanic Name* - @${data[ii].mechanic_name}\n\n*Customer Name* - ${data[ii].customer_name}\n\n*Lead Status* - ${data[ii].status}\n\n*Appointment Date* - ${data[ii].appointment_date}\n\n*Lead Description* - \n${data[ii].lead_description}\n`
+        }
+      },
+      {
+        "type": "divider"
+      },
+      {
+        "type": "actions",
+        "elements": [
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Accept",
+              "emoji": true
+            },
+            "style": "primary",
+            "value": "reject_lead"
+          },
+          {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Reject",
+              "emoji": true
+            },
+            "value": "accept_lead"
+          }
+        ]
+      },
+      {
+        "type": "divider"
+      }
+    ])
+  }
   let blocks =  [
 		{
 			"type": "section",
@@ -38,7 +92,8 @@ const displayHome = async (user, data) => {
 				}
 			]
 		}
-	];
+  ];
+  blocks = blocks.concat(dynamicBlock);
   let view = {
     type: "home",
     title: {
@@ -62,143 +117,161 @@ const displayHome = async (user, data) => {
   }
 };
 
+const getUsersList = function(){
+  request({
+    url: apiUrl+ '/users.list', //URL to hit
+    qs: {token: SLACK_OAUTH_TOKEN}, //Query string data
+    method: 'GET', //Specify the method
+  }, function (error, response, body) {
+    if (error) {
+      return [];
+    } else {
+      // console.log(JSON.parse(body))
+      return JSON.parse(body);
+    }
+  })
+}
+
 const openCreateTaskModal = async trigger_id => {
-  const modalExpense = {
-    title: {
-      type: "plain_text",
-      text: "Create Lead"
-    },
-    submit: {
-      type: "plain_text",
-      text: "Submit"
-    },
-    type: "modal",
-    close: {
-      type: "plain_text",
-      text: "Cancel"
-    },
-    blocks: [
-      {
-        block_id: "expense_heading",
-        type: "section",
-        text: {
-          type: "plain_text",
-          text:
-            "The following form will create a Lead and send it for approval."
-        }
-      },
-      {
-        type: "divider"
-      },
-      {
-        block_id: "customer_name",
-        type: "input",
-        element: {
-          action_id: "customer_name_id",
-          type: "plain_text_input"
-        },
-        label: {
-          type: "plain_text",
-          text: "Customer Name"
-        }
-      },
-      {
-        type: "divider"
-      },
-      {
-        block_id: "lead_description",
-        type: "input",
-        element: {
-          action_id: "lead_description_id",
-          type: "plain_text_input",
-          multiline: true
-        },
-        label: {
-          type: "plain_text",
-          text: "Lead description"
-        }
-      },
-      {
-        block_id: "lead_date",
-        type: "input",
-        element: {
-          action_id: "lead_date_val",
-          type: "datepicker",
-          initial_date: "2020-01-08",
-          placeholder: {
-            type: "plain_text",
-            text: "Select lead date"
-          }
-        },
-        label: {
-          type: "plain_text",
-          text: "Lead Date"
-        }
-      },
-      {
-        "type": "input",
-        "element": {
-          "type": "static_select",
-          "placeholder": {
-            "type": "plain_text",
-            "text": "Select a mechanic",
-            "emoji": true
-          },
-          "options": [
+  var userList = [], userListBlock = [];
+  request({
+    url: apiUrl+ '/users.list', //URL to hit
+    qs: {token: SLACK_OAUTH_TOKEN}, //Query string data
+    method: 'GET', //Specify the method
+  }, function (error, response, body) {
+    if (error) {
+      return [];
+    } else {
+      var members = JSON.parse(body).members;
+      console.log(members);
+      for(var ii =0; ii < members.length; ii++){
+        if(!members.is_bot && members[ii].profile.display_name ){
+          userList.push(members[ii]);
+          userListBlock.push(
             {
               "text": {
                 "type": "plain_text",
-                "text": "*this is plain_text text*",
+                "text": members[ii].name,
                 "emoji": true
               },
-              "value": "value-0"
-            },
-            {
-              "text": {
-                "type": "plain_text",
-                "text": "*this is plain_text text*",
-                "emoji": true
-              },
-              "value": "value-1"
-            },
-            {
-              "text": {
-                "type": "plain_text",
-                "text": "*this is plain_text text*",
-                "emoji": true
-              },
-              "value": "value-2"
+              "value": members[ii].id
             }
-          ]
-        },
-        "label": {
-          "type": "plain_text",
-          "text": "Label",
-          "emoji": true
+          )
         }
-      },
-      {
-        "type": "context",
-        "elements": [
+      }
+      const modalExpense = {
+        title: {
+          type: "plain_text",
+          text: "Create Lead"
+        },
+        submit: {
+          type: "plain_text",
+          text: "Submit"
+        },
+        type: "modal",
+        close: {
+          type: "plain_text",
+          text: "Cancel"
+        },
+        blocks: [
           {
-            "type": "image",
-            "image_url": "https://api.slack.com/img/blocks/bkb_template_images/placeholder.png",
-            "alt_text": "placeholder"
+            block_id: "expense_heading",
+            type: "section",
+            text: {
+              type: "plain_text",
+              text:
+                "The following form will create a Lead and send it for approval."
+            }
+          },
+          {
+            type: "divider"
+          },
+          {
+            block_id: "customer_name",
+            type: "input",
+            element: {
+              action_id: "customer_name_id",
+              type: "plain_text_input"
+            },
+            label: {
+              type: "plain_text",
+              text: "Customer Name"
+            }
+          },
+          {
+            type: "divider"
+          },
+          {
+            block_id: "lead_description",
+            type: "input",
+            element: {
+              action_id: "lead_description_id",
+              type: "plain_text_input",
+              multiline: true
+            },
+            label: {
+              type: "plain_text",
+              text: "Lead description"
+            }
+          },
+          {
+            block_id: "lead_date",
+            type: "input",
+            element: {
+              action_id: "lead_date_val",
+              type: "datepicker",
+              initial_date: "2020-01-08",
+              placeholder: {
+                type: "plain_text",
+                text: "Select lead date"
+              }
+            },
+            label: {
+              type: "plain_text",
+              text: "Lead Date"
+            }
+          },
+          {
+            block_id: "mechanic",
+            type: "input",
+            element: {
+              action_id: "mechanic_id",
+              type: "static_select",
+              placeholder: {
+                type: "plain_text",
+                text: "Select a mechanic",
+                emoji: true
+              },
+              options: userListBlock
+            },
+            label: {
+              type: "plain_text",
+              text: "Select a mechanic",
+              emoji: true
+            }
+          },
+          {
+            type: "context",
+            elements: [
+              {
+                type: "image",
+                image_url: "https://api.slack.com/img/blocks/bkb_template_images/placeholder.png",
+                alt_text: "placeholder"
+              }
+            ]
           }
         ]
-      }
-      
-      
-    ]
-  };
-
-  const args = {
-    token: process.env.SLACK_BOT_TOKEN,
-    trigger_id: trigger_id,
-    view: JSON.stringify(modalExpense)
-  };
-
-  const result = await axios.post(`${apiUrl}/views.open`, qs.stringify(args));
+      };
+    
+      const args = {
+        token: process.env.SLACK_BOT_TOKEN,
+        trigger_id: trigger_id,
+        view: JSON.stringify(modalExpense)
+      };
+    
+      const result = axios.post(`${apiUrl}/views.open`, qs.stringify(args));
+    }
+  })
 };
 
 module.exports = { displayHome, openCreateTaskModal };
--- index.js
+++ index.js
@@ -5,11 +5,58 @@ var bodyParser = require("body-parser");
 var signature = require("./verifySignature");
 const appHome = require("./appHome");
 const dotenv = require('dotenv');
+const mysql = require('mysql');
 dotenv.config();
 // Store our app's ID and Secret. These we got from Step 1.
 // For this tutorial, we'll keep your API credentials right here. But for an actual app, you'll want to  store them securely in environment variables.
 var clientId = process.env.CLIENT_ID;
 var clientSecret = process.env.CLIENT_SECRET;
+
+const con = mysql.createConnection({  
+  host: "localhost",
+  user: "root",  
+  password: "",
+  database: "leader"
+});
+
+const createDBTables = function(){
+  let createTodos = `create table if not exists leads(
+    id int primary key auto_increment,
+    customer_name varchar(255) not null,
+    lead_description varchar(255) not null,
+    mechanic_name varchar(255) not null,
+    mechanic_slack_id varchar(255) not null,
+    created_by_name varchar(255) not null,
+    created_by_slack_id varchar(255) not null,
+    status varchar(255) not null,
+    appointment_date varchar(255) not null,
+    created_at varchar(255) not null
+  )`;
+  con.query(createTodos, function(err, results, fields) {
+    if (err) {
+      console.log(err.message);
+    }else{
+      console.log("Table Created");
+    }
+  });
+}
+
+con.connect(function(err) {
+  if (err){
+    con.query("CREATE DATABASE leader", function (err, result) {  
+      if (err){
+        throw err;  
+      }else{
+        createDBTables();
+        console.log("Database created");  
+      }
+    });  
+    throw err;  
+  }else{
+    // createDBTables();
+    console.log("Connected!");  
+  }
+});   
 
 // Instantiates Express and assigns our app variable to it
 var app = express();
@@ -30,9 +77,45 @@ app.get('/', function(req, res) {
 });
 
 app.post("/slack/actions", async (req, res) => {
-  const { token, trigger_id, user, actions, type } = JSON.parse(req.body.payload);
+  const { token, trigger_id, user, actions, type, view } = JSON.parse(req.body.payload);
   if (actions && actions[0].action_id.match(/create_task/)) {
     appHome.openCreateTaskModal(trigger_id);
+  }
+  else if(type === 'view_submission') {
+    res.send(''); // Make sure to respond to the server to avoid an error
+    var ts = new Date();
+    console.log("Insert Running");
+    var mechanic = view.state.values.mechanic.mechanic_id.selected_option;
+    const data = {
+      timestamp: ts,
+      customerName: view.state.values.customer_name.customer_name_id.value,
+      leadDate: view.state.values.lead_date.lead_date_val.selected_date,
+      leadDecription: view.state.values.lead_description.lead_description_id.value,
+      mechanicId: mechanic.value,
+      mechanicName: mechanic.text.text,
+      creatorName: user.name,
+      creatorId: user.id,
+      status: "new"
+    }
+    let insertLeadQuery = `INSERT INTO leads(customer_name, lead_description, mechanic_name, mechanic_slack_id, created_by_name, created_by_slack_id, status, appointment_date, created_at)
+           VALUES('${data.customerName}', '${data.leadDecription}', '${data.mechanicName}', '${data.mechanicId}', '${data.creatorName}', '${data.creatorId}', '${data.status}', '${data.leadDate}', '${data.timestamp}')`;
+
+    con.query(insertLeadQuery, function(err, results, fields) {
+      if (err) {
+        appHome.displayHome(user.id);
+        console.log(err.message);
+      }else{
+        var getLeadsQuery = 'Select * from leads ORDER BY id DESC;'
+        con.query(getLeadsQuery, function(err, results, fields) {
+          if (err) {
+            console.log("Fetch Lead Error");
+          }else{
+            appHome.displayHome(user.id, results);
+          }
+        });
+        console.log("Table Created");
+      }
+    });
   }
 });
 
@@ -84,7 +167,17 @@ app.post("/slack/events", async (req, res) => {
         // Triggered when the App Home is opened by a user
         if (type === "app_home_opened") {
           // Display App Home
-          appHome.displayHome(user);
+          var getLeadsQuery = 'Select * from leads ORDER BY id DESC;'
+          con.query(getLeadsQuery, function(err, results, fields) {
+            if (err) {
+              console.log("Fetch Lead Query Error");
+            }else{
+              // console.log(fields);
+              // console.log(results);
+              appHome.displayHome(user, results);
+            }
+          });
+          // appHome.displayHome(user);
         }
 
         /* 
